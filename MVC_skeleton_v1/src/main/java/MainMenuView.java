cef7062aab9cb607c15fdd8dadb0695d7ee407be
Salim <17050111014@ybu.edu.tr>
2021-02-03 12:57:46 +0300

    Project added

---
+++ MVC_skeleton_v1/src/main/java/MainMenuView.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class MainMenuView implements ViewInterface {
+
+	@Override
+	public ViewData create(ModelData modelData, String functionName, String operationName) throws Exception {
+
+		Integer choice;
+		do {
+			System.out.println("1. New Application");
+			System.out.println("2. Application Inquiry");
+			System.out.println("3. Personnel Login");
+			System.out.println("4. Quit");
+			System.out.println();
+
+			choice = getInteger("Enter your choice : ", false);
+		} 
+		while (choice == null || choice < 1 || choice > 6);
+		
+		
+		Map<String, Object> userInput = new HashMap<>();
+		userInput.put("mainMenuChoice", choice);
+		
+		switch (choice.intValue()) {
+		case 1: operationName = "select";		break;
+		case 2: operationName = "select.gui";	break;
+		case 3: operationName = "insert.gui";	break;
+		case 4: operationName = "update.gui";	break;
+		case 5: operationName = "delete.gui";	break;
+		default: return new ViewData(null, null);
+		}
+		
+		return new ViewData("Department", operationName, new HashMap<>());
+	}
+
+	@Override
+	public String toString() {
+		return "Main Menu View";
+	}		
+}
---
+++ MVC_skeleton_v1/src/main/java/ModelViewControllerConsole.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+
+public class ModelViewControllerConsole {
+
+	public static void main(String[] args) throws Exception {
+		// Connect to database
+		connectToDatabase();
+
+		
+		// Model View Controller (MVC)
+		// Router knows all the controllers
+		Map<String, Controller> router = new HashMap<>();		
+		router.put("MainMenu", new Controller(new MainMenuView(), new NopModel()));
+		router.put("Department", new Controller(new DepartmentView(), new DepartmentModel()));
+
+		ViewData viewData = new ViewData("MainMenu", "");		
+		do {
+			// Model, View, and Controller
+			Controller controller = router.get(viewData.functionName);
+			ModelData modelData = controller.executeModel(viewData);
+			viewData = controller.getView(modelData, viewData.functionName, viewData.operationName);
+			
+			System.out.println();
+			System.out.println("-------------------------------------------------");
+			System.out.println();
+		}
+		while (viewData.functionName != null);
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("Program is ended...");
+
+
+		// Disconnect from database
+		disconnectFromDatabase();
+	}
+
+	
+	public static void connectToDatabase() {
+		DatabaseUtilities.host = "localhost:63664";
+		DatabaseUtilities.databaseName = "AdventureWorks2019";
+		
+		try {
+			DatabaseUtilities.getConnection();
+		}
+		catch(Exception e) {
+			System.out.println("Exception occured : " + e);
+			return;
+		}		
+	}
+	
+	public static void disconnectFromDatabase() {
+		try {
+			DatabaseUtilities.disconnect();
+		}
+		catch(Exception e) {
+			System.out.println("Error disconnecting from database : " + e);
+			return;
+		}		
+	}
+	
+}
